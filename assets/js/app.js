aaa9487b05528dfce76383c3d1ee407fe78cdf81
James V <james.vanhoke@yahoo.com>
2021-03-03 22:56:43 -0700

    Added Timer logic, start button functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,53 @@
-// code quiz
+// Variables
+var timerEl = document.querySelector("#timer");
+var startBtn = document.querySelector("#start-btn")
+
+//Time Remaining
+var timeRemaining = 60;
+
+//Timer countdown function
+function setTime() {
+  var Countdown = setInterval(function () {
+    timeRemaining--;
+    timerEl.textContent = "Time: " + timeRemaining;
+
+    //Timer reaches 0 logic
+    if (timeRemaining === 0) {
+      clearInterval(Countdown);
+      console.log("You Lost!");
+    }
+  }, 1000);
+}
+
+// Listen for a click event on start button
+startBtn.addEventListener("click", function(){
+    //Hides start-page element
+    document.getElementById("start-page").style.display = "none"
+    //show question-page element
+    document.getElementById("question-page").style.display = "inline-block"
+    //Runs countdown timer
+})
+
+startBtn.addEventListener("click", setTime())
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 // LOGIC - JS
 // click the start button
-//     timer starts 
+//     timer starts
 //     I am presented with a question
 
 // answer a question
@@ -17,7 +60,6 @@
 //     game over
 //         ask for user initials
 //         save initial and score
-
 
 // HTML area
 // Start Page
@@ -33,14 +75,9 @@
 //         d)
 // Score Page
 
-
-
-
-
-
 // PROGRAM LOGIC
 // start button
-//     a timer starts  
+//     a timer starts
 //     presented with question1 text
 //     presented with question1 answer buttons
 
@@ -59,7 +96,7 @@
 
 // SECTIONS
 // Main page
-//     Timer area
+//     Timer area ✅
 //     Question area
 //     Link to view High Scores
 // High Scores Page
